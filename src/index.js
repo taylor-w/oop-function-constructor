381a5f53a9c0492365d08d3a12827cc79f397d7e
Taylor Wood <taylorwood39@gmail.com>
2020-11-05 20:23:55 -0600

    Add new class via extends

--- src/index.js
+++ src/index.js
@@ -1,25 +1,40 @@
 const Person = require("./Person");
 
-class Student extends Person {
-  constructor({ name, age, occupation, hobbies, major }) {
+class Faculty extends Person {
+  constructor({ name, age, occupation, hobbies, wage }) {
     super({ name, age, occupation, hobbies });
-    this.major = major;
+    this.wage = wage;
   }
 }
 
-const matt = new Student({
-  name: "Matt",
-  age: 30,
-  occupation: "Frontend Dev",
-  hobbies: ["Destiny", "GameDev"],
-  major: "CIS",
-});
+// class Student extends Person {
+//   constructor({ name, age, occupation, hobbies, major }) {
+//     super({ name, age, occupation, hobbies });
+//     this.major = major;
+//   }
+// }
+
+// const matt = new Student({
+//   name: "Matt",
+//   age: 30,
+//   occupation: "Frontend Dev",
+//   hobbies: ["Destiny", "GameDev"],
+//   major: "CIS",
+// });
 
 const tay = new Person({
   name: "Taylor",
   age: 39,
   occupation: "Programmer",
   hobbies: ["Fishing", "Gaming"],
+});
+
+const lawrence = new Faculty({
+  name: "Appelbaum",
+  age: 45,
+  occupation: "Professor",
+  hobbies: ["Radio", "Fishing"],
+  wage: 60000,
 });
 
 tay.listHobbies();
@@ -31,4 +46,5 @@ console.log(tay.info);
 tay.newHobby = "Training";
 tay.listHobbies();
 
-console.log(matt);
+// console.log(matt);
+console.log(lawrence);
