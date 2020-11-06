e95d348b4a6559eb0c351f829e09c19b6352f8f1
Taylor Wood <taylorwood39@gmail.com>
2020-11-05 20:19:20 -0600

    Add new class via extends

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 const Person = require("./Person");
 
+class Student extends Person {
+  constructor({ name, age, occupation, hobbies, major }) {
+    super({ name, age, occupation, hobbies });
+    this.major = major;
+  }
+}
+
+const matt = new Student({
+  name: "Matt",
+  age: 30,
+  occupation: "Frontend Dev",
+  hobbies: ["Destiny", "GameDev"],
+  major: "CIS",
+});
+
 const tay = new Person({
   name: "Taylor",
   age: 39,
@@ -15,3 +30,5 @@ console.log(tay.info);
 // use setter
 tay.newHobby = "Training";
 tay.listHobbies();
+
+console.log(matt);
